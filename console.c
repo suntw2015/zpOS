61a9b83215e121e4df2cbc26011b4955c835914a
suntianwen002 <suntianwen002@ke.com>
2023-09-04 15:22:05 +0800

    fix

--- console.c
+++ console.c
@@ -18,36 +18,51 @@ void printc(char c)
     u8 foreColour = 15;
 
     //2个字节表示一个字符，0-7 ascll码值，8-11前景色，12-15背景色
-    u16 charAttr = (0x0F << 8) | c;
+    u8  attributeByte = (backColour << 4) | (foreColour & 0x0F);
+    // The attribute byte is the top 8 bits of the word we have to send to the
+    // VGA board.
+    u16 attribute = attributeByte << 8;
+    u16 *location;
 
-    if (c == '\n') {
-        //换行
-        cursor_x++;
-        cursor_y=0;
-    } else if (c == 0x7f) {
-        //删除
-        if (cursor_x==0 && cursor_y==0) {
-            return;
-        }
-        //当前位置用空白字符
-        *(video_address+cursor_x*80+cursor_y) = blank;
-        //设置坐标
-        if (cursor_y > 0) {
-            cursor_y--;
-        } else {
-            cursor_y = screen_width-1;
-            if (cursor_x) {
-                cursor_x--;
-            }
-        }
-    } else if (c>=0x20 && c <0x7f) {
-        //可打印字符
-        *(video_address+cursor_x*80+cursor_y) = charAttr;
+    // Handle a backspace, by moving the cursor back one space
+    if (c == 0x08 && cursor_x)
+    {
+        cursor_x--;
+    }
+
+    // Handle a tab by increasing the cursor's X, but only to a point
+    // where it is divisible by 8.
+    else if (c == 0x09)
+    {
+        cursor_x = (cursor_x+8) & ~(8-1);
+    }
+
+    // Handle carriage return
+    else if (c == '\r')
+    {
+        cursor_x = 0;
+    }
+
+    // Handle newline by moving cursor back to left and increasing the row
+    else if (c == '\n')
+    {
+        cursor_x = 0;
         cursor_y++;
-        if (cursor_y == screen_width) {
-            cursor_x++;
-            cursor_y=0;
-        }
+    }
+    // Handle any other printable character.
+    else if(c >= ' ')
+    {
+        location = video_address + (cursor_y*80 + cursor_x);
+        *location = c | attribute;
+        cursor_x++;
+    }
+
+    // Check if we need to insert a new line because we have reached the end
+    // of the screen.
+    if (cursor_x >= 80)
+    {
+        cursor_x = 0;
+        cursor_y ++;
     }
 
     scroll_console();
@@ -87,6 +102,7 @@ void scroll_console()
 {
     // Get a space character with the default colour attributes.
     u8 attributeByte = (0 /*black*/ << 4) | (15 /*white*/ & 0x0F);
+    u16 blank = 0x20 /* space */ | (attributeByte << 8);
 
     // Row 25 is the end, this means we need to scroll up
     if(cursor_y >= screen_height)
