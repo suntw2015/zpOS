0875d7df1161d42be012c63878f59830f98e569f
suntianwen002 <suntianwen002@ke.com>
2023-09-12 14:25:18 +0800

    fix

--- kernel.asm
+++ kernel.asm
@@ -3,22 +3,23 @@ global _start
 [BITS 32]
 [extern main]
 
+helloKernel db 'Start Kernel', 0
+
 _start:
-        ; 将字符串的字符逐个输出
-        mov edx, helloKernel  ; 字符串地址
-        mov ebx, 0xB8000  ; 显存地址
+    ; 将字符串的字符逐个输出
+    mov edx, helloKernel ; 字符串地址
+    mov edi, 0xB8000    ; 显存地址
 
 print_loop:
-        mov al, dl
-        mov ah, 0x0F
-        mov [ebx], ax
-        add ebx, 2
-        add edx, 1
-        test al, al  ; 检查AL寄存器的值是否为0，即字符串是否结束
-        jz print_end  ; 如果是0，跳转到调用main函数的标签
-        jmp print_loop  ; 继续下一个字符的打印
-print_end:
-	call main
-	jmp $
+    mov al, [edx]       ; 读取字符串中的一个字符
+    mov ah, 0x0F        ; 字符属性
+    mov word [edi], ax  ; 将字符和属性存储到显存中
+    add edi, 2          ; 递增显存地址
+    add edx, 1          ; 递增字符串地址
+    test al, al         ; 检查AL寄存器的值是否为0，即字符串是否结束
+    jz print_end        ; 如果是0，跳转到调用main函数的标签
+    jmp print_loop      ; 继续下一个字符的打印
 
-helloKernel: db 'Start Kenel',0
+print_end:
+    call main
+    jmp $
