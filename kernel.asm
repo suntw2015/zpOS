8fd80376cc359dd635ab4a8400710d84c3defcad
suntianwen002 <suntianwen002@ke.com>
2023-09-12 12:27:14 +0800

    fix

--- kernel.asm
+++ kernel.asm
@@ -5,15 +5,15 @@ global _start
 
 _start:
         ; 将字符串的字符逐个输出
-        mov dx, helloKernel  ; 字符串地址
-        mov bx, 0xB8000  ; 显存地址
+        mov edx, helloKernel  ; 字符串地址
+        mov ebx, 0xB8000  ; 显存地址
 
 print_loop:
         mov al, dl
         mov ah, 0x0F
-        mov [bx], ax
-        add bx, 2
-        add dx, 2
+        mov [ebx], ax
+        add ebx, 2
+        add edx, 1
         test al, al  ; 检查AL寄存器的值是否为0，即字符串是否结束
         jz print_end  ; 如果是0，跳转到调用main函数的标签
         jmp print_loop  ; 继续下一个字符的打印
